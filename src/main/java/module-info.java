21103d2ed935f4f26f1042b5e44da66db0aa9136
Rodrigo Merino <elrodro83@users.noreply.github.com>
2023-04-05 16:08:32 -0300

    W-12683049: Generate a module-info.java for policy-api module (#72)

---
+++ src/main/java/module-info.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+/**
+ * Mule module Policy API.
+ * 
+ * @moduleGraph
+ * @since 1.6
+ */
+module org.mule.runtime.policy.api {
+  
+  requires org.mule.runtime.api;
+  
+  exports org.mule.runtime.policy.api;
+
+}
